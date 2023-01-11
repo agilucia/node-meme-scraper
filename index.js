62f549d0529cd9e8e215d08f7f49dc2e32833274
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-11 16:32:50 +0100

    Get html through cheerio

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
 
 const website = new URL('https://memegen-link-examples-upleveled.netlify.app/');
 
 const response = await fetch(website);
 const body = await response.text();
 
-console.log(body);
+//console.log(body);
+
+const $ = cheerio.load(body);
+
+//console.log($);
+
+$.html();
+
+console.log($.html());
