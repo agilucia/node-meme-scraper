239954576587d6fc1ea34e9d4bd2d68e7f8df454
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-11 15:46:09 +0100

    Add node-fetch

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+import fetch from 'node-fetch';
+
+const website = new URL('https://memegen-link-examples-upleveled.netlify.app/');
+
+const response = await fetch(website);
+const body = await response.text();
+
+console.log(body);
