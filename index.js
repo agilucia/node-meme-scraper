3670cac53ef6bc319babd675cfcebbddab526757
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-12 15:58:55 +0100

    Get first ten src

--- index.js
+++ index.js
@@ -10,12 +10,27 @@ const body = await response.text();
 
 const $ = cheerio.load(body);
 
+const html = $(`img`).html('src'); // get first src attribute from img
+
+// console.log(html);
+
+// push first 10 image urls to array:
+const imageUrls = [];
+
+for (let i = 0; i <= 9; i++) {
+  imageUrls.push(html[i].attribs.src);
+}
+
+console.log(imageUrls);
+
 // console.log($);
 
-const htmlContent = $.html();
+// const htmlContent = $.html();
 
 // console.log(htmlContent);
 
-const matchingElements = $.html('img');
+// const matchingElements = $.html('img');
 
-console.log(matchingElements);
+// console.log(matchingElements);
+
+// const imageUrls = new Array();
